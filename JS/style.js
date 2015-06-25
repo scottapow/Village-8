9d33870c37cfb22038298b91fbfafb7840919f13
scottapow <lfh.scot@gmail.com>
2015-06-24 23:31:57 -0400

    Added Slick carousel and body elements

---
+++ JS/style.js
@@ -0,0 +1,29 @@
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        variableWidth: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        variableWidth: true
+      }
+    }
+  ]
+});
