dcb16c741eba0cc507bd50ef2eddd0499ae3c2cc
scottapow <lfh.scot@gmail.com>
2015-06-29 23:02:25 -0400

    fixed ajax pulling imdv info from api

--- JS/style.js
+++ JS/style.js
@@ -1,26 +1,33 @@
 
 // Movie Synops Content //
 
-// Store IMDB id's
+  // Store IMDB id's
 var $movie1ID = $('.synops div:nth-child(1)').attr('id');
 var $movie2ID = $('.synops div:nth-child(2)').attr('id');
 var $movie3ID = $('.synops div:nth-child(3)').attr('id');
 var $movie4ID = $('.synops div:nth-child(4)').attr('id');
 var $movie5ID = $('.synops div:nth-child(5)').attr('id');
 
-console.log($movie1ID + $movie2ID + $movie3ID + $movie4ID + $movie5ID);
-
-$.ajax({
-  url:       'http://www.myapifilms.com/imdb',
-  dataType:  'jsonp',
-  success:   function (response) {
+function getMovie(imdbid) {
+  $.ajax({    
+    data:  {
+      idIMDB: imdbid
+    },
+    method: 'GET',
+    url:        'http://www.myapifilms.com/id?format=jsonp',
+    dataType: "jsonp",
+    success: function (response) {
       console.log(response);
-   }
-});
+    }
+  }); 
+}
 
-// Token: 04952d4f-cb8c-47f2-943e-a01e19bcfed6
+getMovie($movie1ID);
 
-// Slick Carousel //
+//Token: 04952d4f-cb8c-47f2-943e-a01e19bcfed6
+// URL: http://www.myapifilms.com/imdb/inTheaters?format=JSON&lang=en-us&token=4952d4f-cb8c-47f2-943e-a01e19bcfed6
+
+/* * * * * Slick Carousel * * * * */
 
 $('.center').slick({
   centerMode: true,
