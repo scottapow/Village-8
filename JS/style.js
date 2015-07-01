e4b3de0a1795f5c38847b15a4e25b8ee82e4f8d4
scottapow <lfh.scot@gmail.com>
2015-06-30 21:28:12 -0400

    Added imdb API CSS styling to divs

--- JS/style.js
+++ JS/style.js
@@ -18,11 +18,40 @@ function getMovie(imdbid) {
     dataType: "jsonp",
     success: function (response) {
       console.log(response);
+      var imgSrc = response.urlPoster;
+      $('#' + imdbid).html( 
+        "<img src=\'" + imgSrc + "\' >" +
+        "<h1>" + response.title + "</h1><br>" +
+        "<p>" + response.plot + "</p>"
+        );
+      $('#' + imdbid + ' img').css({
+        "display": "inline-block", 
+        "float": "left",
+        "width": "142px",
+        "height": "226px",
+        "margin-bottom": "40px",
+        "padding-right": "10px" 
+      });
+      $('#' + imdbid + ' h1').css({
+        "display": "inline-block", 
+        "position": "relative",
+        "margin-left": "10px"
+      });
+      $('#' + imdbid + ' > p').css({
+        "display": "inline",
+        "position": "relative", 
+        "margin-top": "10px",
+        "clear": "right"
+      });
     }
   }); 
 }
 
 getMovie($movie1ID);
+getMovie($movie2ID);
+getMovie($movie3ID);
+getMovie($movie4ID);
+getMovie($movie5ID);
 
 //Token: 04952d4f-cb8c-47f2-943e-a01e19bcfed6
 // URL: http://www.myapifilms.com/imdb/inTheaters?format=JSON&lang=en-us&token=4952d4f-cb8c-47f2-943e-a01e19bcfed6
